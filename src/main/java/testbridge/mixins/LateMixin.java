6925a6eb7fb9f487cbe3818ed76bc0b83b3ac54b
Korewa_Li <li.hvktqs@gmail.com>
2022-10-24 21:02:33 +0700

    Prepair Mixin stuff

---
+++ src/main/java/testbridge/mixins/LateMixin.java
@@ -0,0 +1,28 @@
+package testbridge.mixins;
+
+import com.google.common.collect.ImmutableList;
+import net.minecraftforge.fml.common.Loader;
+import zone.rong.mixinbooter.ILateMixinLoader;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LateMixin implements ILateMixinLoader {
+
+  public static final List<String> modMixins = ImmutableList.of("appliedenergistics2", "refinedstorage", "logisticspipes");
+
+  @Override
+  public List<String> getMixinConfigs() {
+    return modMixins.stream().map(mod -> "mixin.testbridge." + mod + ".json").collect(Collectors.toList());
+  }
+
+  @Override
+  public boolean shouldMixinConfigQueue(String mixinConfig) {
+    String[] parts = mixinConfig.split("\\.");
+    return parts.length != 4 || shouldEnableModMixin(parts[2]);
+  }
+
+  public boolean shouldEnableModMixin(String mod) {
+    return Loader.isModLoaded(mod);
+  }
+}
--- src/main/java/testbridge/mixins/logisticspipes/TB_MixinPipeInfoProvider.java
+++ src/main/java/testbridge/mixins/logisticspipes/TB_MixinPipeInfoProvider.java
@@ -1,9 +1,6 @@
-package testbridge.mixin.TOPCompat;
+package testbridge.mixins.logisticspipes;
 
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import javax.annotation.Nonnull;
 
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
@@ -12,6 +9,14 @@ import net.minecraft.world.World;
 import mcjty.theoneprobe.api.IProbeHitData;
 import mcjty.theoneprobe.api.IProbeInfo;
 import mcjty.theoneprobe.api.ProbeMode;
+
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import logisticspipes.kotlin.jvm.internal.Intrinsics;
 import logisticspipes.kotlin.text.StringsKt;
@@ -22,18 +27,20 @@ import network.rs485.logisticspipes.compat.TheOneProbeIntegration;
 
 import testbridge.pipes.ResultPipe;
 
-import javax.annotation.Nonnull;
-
 @Mixin(targets = "network.rs485.logisticspipes.compat.TheOneProbeIntegration$PipeInfoProvider")
-public class MixinPipeInfoProvider {
+public abstract class TB_MixinPipeInfoProvider {
+  @Final
+  @Unique
+  String prefix = "top.testbridge.";
 
   @Inject(
-      method = "addProbeInfo",
-      at = @At(
-          value = "INVOKE",
-          target = "Llogisticspipes/kotlin/jvm/internal/Intrinsics;checkNotNullParameter(Ljava/lang/Object;Ljava/lang/String;)V",
-          shift = At.Shift.AFTER)
-          )
+    method = "addProbeInfo",
+    at = @At(
+      value = "INVOKE",
+      target = "Llogisticspipes/kotlin/jvm/internal/Intrinsics;checkNotNullParameter(Ljava/lang/Object;Ljava/lang/String;)V",
+      ordinal = 1,
+      shift = At.Shift.AFTER),
+    cancellable = true)
   public void preAddProbeInfo(
       ProbeMode mode, IProbeInfo probeInfo, EntityPlayer player, World world, IBlockState blockState, IProbeHitData data, CallbackInfo ci) {
     if (probeInfo != null && blockState != null && data != null) {
@@ -42,20 +49,23 @@ public class MixinPipeInfoProvider {
         if (pipe instanceof ResultPipe) {
           Intrinsics.checkNotNullExpressionValue(pipe, "pipe");
           this.addResultPipeInfo((ResultPipe) pipe, probeInfo);
+          ci.cancel();
         }
       }
     }
   }
 
+  @Shadow(aliases = "TheOneProbeIntegration")
+  public TheOneProbeIntegration this$0;
+
   public void addResultPipeInfo(@Nonnull ResultPipe pipe, IProbeInfo probeInfo) {
     String resultPipeName = pipe.getSatellitePipeName();
     if (!StringsKt.isBlank(resultPipeName)) {
-      TheOneProbeIntegration.LPText var4 = new TheOneProbeIntegration().new LPText("top.testbridge.pipe.result.name");
+      TheOneProbeIntegration.LPText var4 = this$0.new LPText(prefix + "pipe.result.name");
       var4.getArguments().add(resultPipeName);
       probeInfo.element(var4);
     } else {
-      probeInfo.element(new TheOneProbeIntegration().new LPText("top.testbridge.pipe.result.no_name"));
+      probeInfo.element(this$0.new LPText(prefix + "pipe.result.no_name"));
     }
   }
-
 }
